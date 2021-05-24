58a41e9f1992ac6f439c8a42a099f06608c546d4
Muhammad Younus Raza <younusraza909@gmail.com>
2021-05-24 22:46:55 +0500

    Integrating Handle Bars

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 
 //Load Config File
 dotenv.config({ path: "./config/config.env" });
@@ -15,6 +16,13 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+//Handlebars
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+//Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+//@desc  LOGIN/Landing page
+//@route GET /
+router.get("/", (req, res) => {
+  res.render("login");
+});
+
+//@desc  Dashboard
+//@route GET /
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+module.exports = router;
