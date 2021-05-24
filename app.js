77f57efef59808f1eabcaf826aef30252633d742
Muhammad Younus Raza <younusraza909@gmail.com>
2021-05-24 22:28:39 +0500

    Implementing morgan

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const morgan = require("morgan");
 
 //Load Config File
 dotenv.config({ path: "./config/config.env" });
@@ -9,6 +10,11 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 const app = express();
+
+//connecting morgan to our app
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(
