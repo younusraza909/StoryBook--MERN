747b34f3e8bb64acd74ab8e3bb3ecf666ce12a08
Muhammad Younus Raza <younusraza909@gmail.com>
2021-05-25 22:18:31 +0500

    Added single story functionality and user story

--- routes/stories.js
+++ routes/stories.js
@@ -39,6 +39,29 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
+// @desc    Show single story
+// @route   GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user._id != req.user.id && story.status == "private") {
+      res.render("error/404");
+    } else {
+      res.render("stories/show", {
+        story,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
+  }
+});
+
 //@desc  show edit page
 //@route GET /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
@@ -100,6 +123,26 @@ router.delete("/:id", ensureAuth, async (req, res) => {
   } catch (err) {
     console.error(err);
     return res.render("error/500");
+  }
+});
+
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: "public",
+    })
+      .populate("user")
+      .lean();
+
+    res.render("stories/index", {
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
   }
 });
 
