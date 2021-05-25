62c5752fdc2bb8df010a9d01a6f71a57840a4d68
Muhammad Younus Raza <younusraza909@gmail.com>
2021-05-25 15:20:07 +0500

    Used handlebars template to render stories on dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const Story = require("../models/Story");
 
 //@desc  LOGIN/Landing page
 //@route GET /
@@ -12,10 +13,18 @@ router.get("/", ensureGuest, (req, res) => {
 
 //@desc  Dashboard
 //@route GET /
-router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard", {
-    name: req.user.firstName,
-  });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    //we use .lean function so that we can pass data comming from mongoose to handlebars as plain JS
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
