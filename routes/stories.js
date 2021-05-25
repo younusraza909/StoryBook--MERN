2abf97ed21f7595a2d0720f2a060268beaac98ef
Muhammad Younus Raza <younusraza909@gmail.com>
2021-05-25 20:01:29 +0500

    Add Public stories route and template view

--- routes/stories.js
+++ routes/stories.js
@@ -24,4 +24,19 @@ router.post("/", ensureAuth, async (req, res) => {
   }
 });
 
+//@desc  Show All Stories
+//@route GET /stories
+router.get("/", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
+    res.render("./stories/index", { stories });
+  } catch (error) {
+    console.log(error);
+    res.render("error/500");
+  }
+});
+
 module.exports = router;
