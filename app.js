adb5fefe220167e5826b2ea81ca6031472c0f6ae
Muhammad Younus Raza <younusraza909@gmail.com>
2021-05-25 07:01:43 +0500

    Added Login functionality using passport js

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const connectDB = require("./config/db");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const path = require("path");
+const passport = require("passport");
+const session = require("express-session");
 
 //Load Config File
 dotenv.config({ path: "./config/config.env" });
+
+//Passport config
+require("./config/passport")(passport);
 
 //Connecting to database
 connectDB();
@@ -22,11 +27,25 @@ if (process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+//Sessions Middleware
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Setting Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(
